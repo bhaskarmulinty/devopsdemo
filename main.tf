9128e59bdcdbc77b7f3be42c1ea097b5080201e8
bhaskarmulinty <73287046+bhaskarmulinty@users.noreply.github.com>
2022-04-04 18:16:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
-provider "aws" {
-   
- }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.74.0"
+    }
+  }
+}
 
 resource "aws_instance" "instance" {
   ami                         = "${var.instance-ami}"
